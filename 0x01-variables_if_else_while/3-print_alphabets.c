fedb492a55962f12b0f751daea5ca0c37ceb3112
Kwame Daniel <lampary78@gmail.com>
2023-03-17 17:54:59 -0700

    lower & uppercase alphabets-fix0

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Print alphabets in lowercase and uppercase
+ * main - Print alphabets in lowercase &  uppercase
  *
  * Return: 0 success
  */
 int main(void)
 {
-        char alpha[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char alpha[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
         int i;
 
         for (i = 0; i < 52; i++)
