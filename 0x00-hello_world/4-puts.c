9df83c2c778e854fe867181a00cc892e49e483ca
Kwame Daniel <lampary78@gmail.com>
2023-03-18 16:12:38 -0700

    puts()-fix)

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,13 @@
-#!/bin/bash
-gcc puts.c 
+#include <stdio.h>
+
+/**
+ * main - print a string
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
