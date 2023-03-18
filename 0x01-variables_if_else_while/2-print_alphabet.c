fecae16b3f4874d818e1c54e66b56e19c416847d
Kwame Daniel <lampary78@gmail.com>
2023-03-17 17:36:47 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Print alphabets in lowercase
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	char alpha[26] = "abcdefghigklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alpha[i]);
+	}
+	putchar('\n');
+	return (0);
+}
