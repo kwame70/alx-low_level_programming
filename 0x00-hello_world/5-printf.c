5cd58534a429d355f27c3331631461a7a81cffea
Kwame Daniel <lampary78@gmail.com>
2023-03-18 16:18:20 -0700

    printf()

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,13 @@
-#!/bin/bash
-gcc -Wall grammar.c
+#include <stdio.h>
+
+/**
+ * main - print a string
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
