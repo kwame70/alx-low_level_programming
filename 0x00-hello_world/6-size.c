117585f3fe49b84db159ccf29fb25bc3e8d09885
Kwame Daniel <lampary78@gmail.com>
2023-03-20 07:08:41 -0700

    sizeof()-bettydoc-fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,18 +5,19 @@
  *
  * Return: 0(success)
  */
-int main (void)
+int main(void)
 {
 	char i;
 	int n;
 	long int l_int;
 	long long int ll_int;
 	float f;
+
 	printf("Size of a char: %d byte(s)\n", sizeof(i));
 	printf("Size of an int: %d byte(s)\n", sizeof(n));
 	printf("Size of a long int: %d byte(s)\n", sizeof(l_int));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(ll_int));
 	printf("Size of a float: %d byte(s)\n", sizeof(f));
 
-	return 0;
+	return (0);
 }
