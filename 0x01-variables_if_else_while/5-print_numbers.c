7c1909491f36e2f657efbdae490d1bc39541aa9d
Kwame Daniel <lampary78@gmail.com>
2023-03-20 15:13:56 -0700

    number 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - printing 0-10
+ *
+ * Return: 0(success)
+ */
+int main(void)
+{	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d\n", i);
+	}
+
+	return (0);
+}
