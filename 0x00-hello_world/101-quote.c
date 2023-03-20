fe6d36b606f54e9c2ab4e6f8a3f9ee47ee7e1ae9
Kwame Daniel <lampary78@gmail.com>
2023-03-20 14:32:28 -0700

    quote-betty-fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,9 +7,9 @@
  */
 int main(void)
 {
-	char output[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char outp[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	fprintf(stdout, "%s", output);
+	fprintf(stdout, "%s", outp);
 
 	return (1);
 }
