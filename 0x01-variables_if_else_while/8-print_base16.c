6caacf22706e33aaa98fead7b3a7fcb1e3d5603d
Kwame Daniel <lampary78@gmail.com>
2023-03-20 18:08:25 -0700

    hex printout

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print hex numbers
+ *
+ * Return: 0(success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	for (i = 97; i < 103; i++)
+	{
+		putchar(i);
+	}
+	
+	putchar('\n');
+
+	return (0);
+}
