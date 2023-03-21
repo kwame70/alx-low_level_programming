892489334c2a284dba72af1646e465f70ad6f2b8
Kwame Daniel <lampary78@gmail.com>
2023-03-21 16:43:16 -0700

    function prototype and header

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main -entry point
+ *
+ * Description: printing char to stdout
+ *
+ * Return: 0(success)
+ */
+int main(void)
+{
+	_putchar('-');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+*This project is about functions and nested lopps in c*
+*Let's get it!*
