59abbaedcc2a151c4b7744c0d9215a138f1ae4e4
Kwame Daniel <lampary78@gmail.com>
2023-04-04 17:27:13 -0700

    first n bytes of memory

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memset - fill memory with constant value
+ *
+ *@s: starting address of memory to be filled
+ *@b: constant byte
+ *@n: number of bytes to be changed
+ *
+ * Return: pointer to memory area
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	for (; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+
+	return (s);
+}
