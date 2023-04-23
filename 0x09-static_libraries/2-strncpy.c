2f56670eb404546000cac07688924b8fce42ce61
Kwame Daniel <lampary78@gmail.com>
2023-04-23 06:07:39 -0700

    static library

---
+++ 0x09-static_libraries/2-strncpy.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include "main.h"
+
+char *_strncpy(char *dest, char *src, int n)
+{
+    return strncpy(dest, src, n);
+}
+
